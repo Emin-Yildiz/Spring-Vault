c3f76a792c6b6433b1075791f1bd016a21f79eab
Emin Yıldız <eminyildiz122@gmail.com>
2024-02-11 14:47:15 +0300

    first commit

---
+++ src/main/java/com/example/vault/controller/VaultController.java
@@ -0,0 +1,22 @@
+package com.example.vault.controller;
+
+import com.example.vault.service.VaultService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vault")
+public class VaultController {
+
+    private final VaultService vaultService;
+
+    public VaultController(VaultService vaultService) {
+        this.vaultService = vaultService;
+    }
+
+    @GetMapping("/")
+    public String getMsg(){
+        return vaultService.getAge();
+    }
+}
